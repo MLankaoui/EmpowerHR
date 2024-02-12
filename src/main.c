1698c378b940c1adad7908f786d00f37b600cad5
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-12 22:55:10 +0100

    welcome message + options dislpayed

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "../includes/main.h"
+#include "../includes/structs.h"
+
+int main(int argc, char **argv)
+{
+	printf("Hello Welcome to EMPOERHR\n");
+	printf("=========================\n");
+	printf("Please mr select one of this options");
+
+	printf("[a] -> Adding new employee details\n");
+	printf("[b] -> Displaying all existing ones\n");
+	return (0);
+}
